f48e98d1713a4e66774c1a6a8ff5703af93f45fd
Harley Dutton <harleydutton@gmail.com>
2023-09-20 05:26:57 -0600

    initial commit

---
+++ Welcome.md
@@ -0,0 +1,6 @@
+Hi, I'm Harley and this is my digital garden.
+
+Take a look:
+- [[Among Thieves Rulebook]]
+- [[Tabula Rasa Rulebook]]
+- [[New York Campaign Notes]]
---
+++ dl_repos.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/ebjohnston/among-thieves.git
+git clone https://github.com/harleydutton/Tabula-Rasa.git
+git clone https://github.com/harleydutton/new-york.git
