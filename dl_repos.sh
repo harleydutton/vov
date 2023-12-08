c2f5a61f8951cb5ca5cd3282b0b15e505161943b
Harley Dutton <harleydutton@gmail.com>
2023-12-07 19:25:28 -0700

    links

--- dl_repos.sh
+++ dl_repos.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/ebjohnston/among-thieves.git
-git clone https://github.com/harleydutton/Tabula-Rasa.git
+git clone https://github.com/harleydutton/tabula-rasa.git
 git clone https://github.com/harleydutton/new-york.git
